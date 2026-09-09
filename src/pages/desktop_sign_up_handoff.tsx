1e6fd373235beda05bf1d779916ceb8637d7c91f
Lucas <257867858+findleyl@users.noreply.github.com>
2026-09-09 16:12:51 -0400

    feat(desktop): hand sign-up off to the system browser
    The desktop shell cannot render the sign-up challenge widget, so opening
    sign-up in the browser lets people finish creating an account and then
    sign in to the app.

---
+++ src/pages/desktop_sign_up_handoff.tsx
@@ -0,0 +1,37 @@
+//
+// Aster Communications Inc.
+//
+// Copyright (c) 2026 Aster Communications Inc.
+//
+// This file is part of this project.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the AGPLv3 as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// AGPLv3 for more details.
+//
+// You should have received a copy of the AGPLv3
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { open_external } from "@/utils/open_link";
+
+const WEB_SIGN_UP_URL = "https://app.astermail.org/register";
+
+export function DesktopSignUpHandoff() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    open_external(`${WEB_SIGN_UP_URL}${window.location.search}`);
+    navigate("/sign-in", { replace: true });
+  }, [navigate]);
+
+  return null;
+}
